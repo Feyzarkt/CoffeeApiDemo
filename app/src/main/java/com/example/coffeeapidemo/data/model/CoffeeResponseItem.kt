9e312d6ce9cbc8b07e78769db476dff57df96853
f00834653 <feyza.urkut1@huawei.com>
2024-02-02 16:40:37 +0300

    Add ingredients parameter and make parcelable

--- app/src/main/java/com/example/coffeeapidemo/data/model/CoffeeResponseItem.kt
+++ app/src/main/java/com/example/coffeeapidemo/data/model/CoffeeResponseItem.kt
@@ -1,6 +1,8 @@
 package com.example.coffeeapidemo.data.model
 
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
@@ -9,11 +11,42 @@ import com.google.gson.annotations.SerializedName
 data class CoffeeResponseItem(
     @PrimaryKey(autoGenerate = true)
     @SerializedName("id")
-    val id: Int,
+    val id: Int? = null,
     @SerializedName("description")
-    val description: String,
+    val description: String? = null,
     @SerializedName("image")
-    val image: String,
+    val image: String? = null,
     @SerializedName("title")
-    val title: String
-)
+    val title: String? = null,
+    @SerializedName("ingredients" )
+    var ingredients : List<String> = listOf()
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        TODO("ingredients")
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(id)
+        parcel.writeString(description)
+        parcel.writeString(image)
+        parcel.writeString(title)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<CoffeeResponseItem> {
+        override fun createFromParcel(parcel: Parcel): CoffeeResponseItem {
+            return CoffeeResponseItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CoffeeResponseItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
