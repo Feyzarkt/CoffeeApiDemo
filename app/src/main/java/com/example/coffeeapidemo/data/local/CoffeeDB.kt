cf0d3a56b61bc092ba4363d950cf06cadde0aced
f00834653 <feyza.urkut1@huawei.com>
2024-02-02 16:39:36 +0300

    Add database converter and update version of table

--- app/src/main/java/com/example/coffeeapidemo/data/local/CoffeeDB.kt
+++ app/src/main/java/com/example/coffeeapidemo/data/local/CoffeeDB.kt
@@ -1,14 +1,13 @@
 package com.example.coffeeapidemo.data.local
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.coffeeapidemo.data.model.CoffeeResponseItem
+import com.example.coffeeapidemo.util.DatabaseConverter
 
-@Database(entities = [CoffeeResponseItem::class], version = 1)
+@Database(entities = [CoffeeResponseItem::class], version = 2)
+@TypeConverters(DatabaseConverter::class)
 abstract class CoffeeDB: RoomDatabase() {
-
     abstract fun coffeeDao() : CoffeeDAO
-
 }
