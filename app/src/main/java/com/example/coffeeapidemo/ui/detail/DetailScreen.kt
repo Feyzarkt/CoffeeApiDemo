9e75f1c6f5e664772b656ddb5e4670e4eedc7c20
f00834653 <feyza.urkut1@huawei.com>
2024-02-05 10:50:41 +0300

    Add detail data and update ui

--- app/src/main/java/com/example/coffeeapidemo/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/coffeeapidemo/ui/detail/DetailScreen.kt
@@ -6,19 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
-import com.example.coffeeapidemo.R
+import com.bumptech.glide.Glide
+import com.example.coffeeapidemo.databinding.FragmentDetailScreenBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailScreen : Fragment() {
 
     private val navArgs: DetailScreenArgs by navArgs()
+    private lateinit var binding: FragmentDetailScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        println("Detail: " + navArgs.item.ingredients.toString())
-        return inflater.inflate(R.layout.fragment_detail_screen, container, false)
+    ): View {
+        binding = FragmentDetailScreenBinding.inflate(inflater)
+        initUI ()
+
+        return binding.root
+    }
+
+    private fun initUI () {
+        val coffeeItem = navArgs.item
+        with(binding) {
+            Glide.with(requireContext()).load(coffeeItem.image).into(ivBackdrop)
+            collapsingToolBar.title = coffeeItem.title
+
+            var ingredients = ""
+            coffeeItem.ingredients.forEach {
+                ingredients += it + "\n"
+            }
+            tvCoffeeIngredients.text = ingredients
+        }
     }
 }
